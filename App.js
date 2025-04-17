78a64fef2f7752cce0652804f97a41147ce80353
FSFitzgerald <nvt472000@gmail.com>
2025-04-17 23:50:27 +0700

    add modal

--- App.js
+++ App.js
@@ -5,17 +5,22 @@ import {
   View,
   TextInput,
   Button,
-  ScrollView,
+  FlatList,
+  Pressable,
+  Modal,
+  Image,
 } from "react-native";
 import { useState } from "react";
 
 export default function App() {
   const [goals, setGoals] = useState([]);
   const [enteredGoal, setEnteredGoal] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
 
   const addGoalHandler = () => {
     setGoals([...goals, enteredGoal]);
     setEnteredGoal("");
+    setModalVisible(false);
   };
 
   const deleteGoalHandler = (index) => {
@@ -23,36 +28,79 @@ export default function App() {
     setGoals(newGoals);
   };
 
+  const startAddGoalHandler = () => {
+    setModalVisible(true);
+  };
+
+  const cancelAddGoalHandler = () => {
+    setModalVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      {/* <StatusBar style="auto" /> */}
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Goal"
-          value={enteredGoal}
-          onChangeText={(text) => setEnteredGoal(text)}
+    <>
+      <StatusBar style="light"  />
+      <View style={styles.container}>
+        <Button
+          title="Add New Goal"
+          color="#a065ec"
+          onPress={startAddGoalHandler}
         />
-        <Button title="Add" onPress={addGoalHandler} />
-      </View>
-      <View style={styles.scrollGoalContainer}>
-        <View style={styles.goalListContainer}>
-          {goals.map((goal, index) => (
-            <View key={index} style={styles.goalContainer}>
-              <View style={styles.goalItem}>
-                <Text>{goal}</Text>
-              </View>
-              <View>
+        <Modal visible={modalVisible} animationType="slide">
+          <View style={styles.inputContainer}>
+            <Image
+              source={require("./assets/images/goal.png")}
+              style={styles.image}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Goal"
+              value={enteredGoal}
+              onChangeText={(text) => setEnteredGoal(text)}
+            />
+            <View style={styles.buttonContainer}>
+              <View style={styles.button}>
                 <Button
-                  title="x"
-                  onPress={() => deleteGoalHandler(index)}
-                ></Button>
+                  title="Add Goal"
+                  onPress={addGoalHandler}
+                  color="#b180f0"
+                />
+              </View>
+              <View style={styles.button}>
+                <Button
+                  title="Cancel"
+                  onPress={cancelAddGoalHandler}
+                  color="#f31282"
+                />
               </View>
             </View>
-          ))}
+          </View>
+        </Modal>
+        <View style={styles.goalContainer}>
+          <FlatList
+            data={goals}
+            renderItem={({ index, item }) => {
+              return (
+                <Pressable
+                  style={styles.goalItemContainer}
+                  android_ripple={{ color: "#210644" }}
+                >
+                  <View style={styles.goalItem}>
+                    <Text style={styles.goalText}>{item}</Text>
+                  </View>
+                  <View style={styles.deleteButton}>
+                    <Button
+                      title="x"
+                      onPress={() => deleteGoalHandler(index)}
+                    ></Button>
+                  </View>
+                </Pressable>
+              );
+            }}
+            keyExtractor={(_, index) => index.toString()}
+          />
         </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -61,52 +109,59 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    // marginTop: 40,
-    // marginBottom: 40,
-    // gap: 20,
+    paddingTop: 20,
   },
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    width: "80%",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    backgroundColor: "#311b6b",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 16,
+  },
+  button: {
+    width: "30%",
+    marginHorizontal: 8,
   },
   input: {
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    color: "#120438",
     borderWidth: 1,
     width: "80%",
     height: "auto",
-    borderRadius: 5,
-    padding: 5,
-  },
-  goalListContainer: {
-    width: "100%",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 10,
-  },
-  scrollGoalContainer: {
-    flex: 5,
-    width: "80%",
+    borderRadius: 6,
+    padding: 16,
   },
   goalContainer: {
+    flex: 5,
+    width: "80%",
+    marginTop: 20,
+  },
+  goalItemContainer: {
     flexDirection: "row",
     width: "100%",
-    backgroundColor: "#7b37c3",
+    backgroundColor: "#5e0acc",
     padding: 10,
     borderRadius: 5,
+    marginBottom: 10,
   },
   goalItem: {
-    width: "90%",
+    width: "85%",
     justifyContent: "center",
     flex: 1,
   },
+  goalText: {
+    color: "white",
+  },
   deleteButton: {
-    width: "10%",
+    width: "15%",
   },
 });
